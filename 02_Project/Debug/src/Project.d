c9ce98095d6fb2f13ab50406dbb92603750ead59
AlexFireBirds <schmidale@hotmail.com>
2018-05-31 17:43:29 +0000

    Initial eclipse project commit

---
+++ 02_Project/Debug/src/Project.d
@@ -0,0 +1,1 @@
+src/Project.d: ../src/Project.cpp
---
+++ 02_Project/src/Project.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Project.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int16_t value;
+	cout << "World" << endl; // prints World
+	cin>>value;
+	return 0;
+}
