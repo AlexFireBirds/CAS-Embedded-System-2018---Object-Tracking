f8d7c4d49ae7b647f20a134f2f32337726397b8c
gheimsch <gheimsch@me.com>
2018-06-01 11:49:53 +0000

    Added Raspberry Camera test to Opencv_Test project

--- 03_Test/Opencv_Test/src/Test.cpp
+++ 03_Test/Opencv_Test/src/Test.cpp
@@ -8,6 +8,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+
 
 using namespace cv;
 int main( int argc, char** argv )
@@ -21,6 +23,37 @@ int main( int argc, char** argv )
     }
   namedWindow( "Display Image", WINDOW_AUTOSIZE );
   imshow( "Display Image", image );
+
+
+  // Raspberry Pi Camera Test
+
+  VideoCapture cap(0);
+  if (!cap.isOpened())
+  {
+	  printf("ERROR: Unable to open the camera\n\r");
+  }
+
+  Mat frame;
+  printf("Start grabbing, press a key on Live window to terminate\n\r");
+  while(1)
+  {
+    cap >> frame;
+    if (frame.empty())
+    {
+    	printf("ERROR: Unable to grab from the camera\n\r");
+        break;
+    }
+    imshow("Live",frame);
+    int key = cv::waitKey(5);
+    key = (key==255) ? -1 : key; //#Solve bug in 3.2.0
+    if (key>=0)
+      break;
+  }
+
+  printf("Closing the camera\n\r");
+  cap.release();
+  destroyAllWindows();
+
   waitKey(0);
   return 0;
 }
