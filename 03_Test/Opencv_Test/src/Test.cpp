d00d7b767f12bb87ba4d0be69bfe6e2c75934f2a
gheimsch <gheimsch@me.com>
2018-06-01 07:19:17 +0000

    Added Opencv test code

---
+++ 03_Test/Opencv_Test/src/Test.cpp
@@ -0,0 +1,27 @@
+/*
+ * Test.cpp
+ *
+ *  Created on: 1 Jun 2018
+ *      Author: pi
+ */
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace cv;
+int main( int argc, char** argv )
+{
+  Mat image;
+  image = imread( argv[1], 1 );
+  if( argc != 2 || !image.data )
+    {
+      printf( "No image data \n" );
+      return -1;
+    }
+  namedWindow( "Display Image", WINDOW_AUTOSIZE );
+  imshow( "Display Image", image );
+  waitKey(0);
+  return 0;
+}
+
+
